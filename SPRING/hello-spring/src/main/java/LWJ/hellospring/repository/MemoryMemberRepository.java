54a33d305e19ce304b9b70e7ef484fda070a133a
wjlee <wjg2nius@gmail.com>
2021-04-29 10:49:41 +0900

    [Study][Spring] Dependency - Component Scan

--- SPRING/hello-spring/src/main/java/LWJ/hellospring/repository/MemoryMemberRepository.java
+++ SPRING/hello-spring/src/main/java/LWJ/hellospring/repository/MemoryMemberRepository.java
@@ -1,9 +1,11 @@
 package LWJ.hellospring.repository;
 
 import LWJ.hellospring.domain.Member;
+import org.springframework.stereotype.Repository;
 
 import java.util.*;
 
+@Repository
 public class MemoryMemberRepository implements MemberRepository{
 
     private static Map<Long, Member> store = new HashMap<>();
--- SPRING/hello-spring/src/main/java/LWJ/hellospring/service/MemberService.java
+++ SPRING/hello-spring/src/main/java/LWJ/hellospring/service/MemberService.java
@@ -3,13 +3,17 @@ package LWJ.hellospring.service;
 import LWJ.hellospring.domain.Member;
 import LWJ.hellospring.repository.MemberRepository;
 import LWJ.hellospring.repository.MemoryMemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 
+@Service
 public class MemberService {
     private final MemberRepository memberRepository;
 
+    @Autowired
     public MemberService(MemberRepository memberRepository) {
         this.memberRepository = memberRepository;
     }
