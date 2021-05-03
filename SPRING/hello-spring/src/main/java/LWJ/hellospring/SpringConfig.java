3c4b3449773f9910adad8ee7fa74141f7e7edb9c
wjlee <wjg2nius@gmail.com>
2021-05-03 15:50:07 +0900

    [Study][Spring] JPA, SpringDataJpa

--- SPRING/hello-spring/src/main/java/LWJ/hellospring/SpringConfig.java
+++ SPRING/hello-spring/src/main/java/LWJ/hellospring/SpringConfig.java
@@ -1,35 +1,49 @@
 package LWJ.hellospring;
 
-import LWJ.hellospring.repository.JdbcMemberRepository;
-import LWJ.hellospring.repository.MemberRepository;
-import LWJ.hellospring.repository.MemoryMemberRepository;
+import LWJ.hellospring.repository.*;
 import LWJ.hellospring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import javax.sql.DataSource;
-
 @Configuration
 public class SpringConfig {
-
+/*
     private DataSource dataSource;
 
     @Autowired
     public SpringConfig(DataSource dataSource) {
         this.dataSource = dataSource;
     }
+*/
+/*
+    private EntityManager em;
+
+    @Autowired
+    public SpringConfig(EntityManager em) {
+        this.em = em;
+    }
+*/
+    private final MemberRepository memberRepository;
+
+    @Autowired
+    public SpringConfig(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
 
     @Bean
     public MemberService memberService() {
-        return new MemberService(memberRepository());
+        return new MemberService(memberRepository);
     }
 
-    @Bean
-    public MemberRepository memberRepository() {
+//    @Bean
+//    public MemberRepository memberRepository() {
         //return new MemoryMemberRepository();
-        return new JdbcMemberRepository(dataSource);
+        //return new JdbcMemberRepository(dataSource);
+        //return new JdbcTemplateMemberRepository(dataSource);
+        //return new JpaMemberRepository(em);
+
         // 추후 return new DbMemoryMemberRepository();
         // Db 만 붙여주면 될 것.
-    }
+//    }
 }
